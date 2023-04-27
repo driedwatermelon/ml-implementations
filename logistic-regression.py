33ee71b3ce4df11269422e528b0f5b4c14eb8f26
Axel Engberg <axelengberg@Axels-MacBook-Pro.local>
2023-04-27 16:18:56 +0300

    First steps.

---
+++ logistic-regression.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class LogisticRegression:
+    
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def train():
+        pass
+
+    def predict():
+        pass
